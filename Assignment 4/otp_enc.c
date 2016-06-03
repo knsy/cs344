cea6e82cdea77cd763ba000bf26072727e1b25c1
knsy <ksy@yakovenko.net>
2016-06-02 20:05:22 -0700

    encoding server and client mostly done

---
+++ Assignment 4/otp_enc.c
@@ -0,0 +1,228 @@
+/*************************************************************
+ * Connects to otp_enc_d and sends a keyfile and a plaintext file
+ * receives cypher.
+ * USAGE:otp_enc plaintext key port
+ * BY: Konstantin Yakovenko
+ * **********************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h> 
+
+
+void error(const char *msg)
+{
+    perror(msg);
+    exit(0);
+}
+
+int main(int argc, char *argv[])
+{
+    int sockfd, portno, n;
+    struct sockaddr_in serv_addr;
+    struct hostent *server;
+	
+    char buffer[256];
+    if (argc < 4) {
+       fprintf(stderr,"usage %s plaintext key port\n", argv[0]);
+       exit(0);
+    }
+    portno = atoi(argv[3]);
+    
+    //open and check files
+     //open file to read
+    int bytesRead = 0;
+    FILE* inFile = fopen(argv[1], "r");
+    if(inFile == NULL){
+		perror("Can't open IN file");
+		exit(1);
+	}
+	
+	//open key to read
+	int bytesKey = 0;
+    FILE* keyFile = fopen(argv[2], "r");
+    if(keyFile == NULL){
+		perror("Can't open KEY file");
+		exit(1);
+	}
+		
+	//measure that KEY is larger than PLAINTEXT
+	fseek( inFile , 0L , SEEK_END);
+	bytesRead = ftell( inFile );
+	rewind( inFile );
+
+	//measure that KEY is larger than PLAINTEXT
+	fseek( keyFile , 0L , SEEK_END);
+	bytesKey = ftell( keyFile );
+	rewind( keyFile );
+	
+	if(bytesRead > bytesKey){
+		perror("KEY is not long enough...");
+		//close files
+		fclose(inFile);
+		fclose(keyFile);
+		exit(1);
+	}
+	
+	bytesRead = 0;
+	bytesKey = 0;
+    
+    
+    
+    
+    //setup the first connection
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) 
+        error("ERROR opening socket");
+    
+    //server = gethostbyname(argv[1]);
+    server = gethostbyname("localhost");
+    if (server == NULL) {
+        fprintf(stderr,"ERROR, no such host\n");
+        exit(0);
+    }
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    bcopy((char *)server->h_addr, 
+         (char *)&serv_addr.sin_addr.s_addr,
+         server->h_length);
+    serv_addr.sin_port = htons(portno);
+    if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
+        error("ERROR connecting");
+	///printf("C:connected....\n");
+        
+    //send 'enc' command to make sure that enc.client cand connect to dec.server
+    //fgets(buffer,255,stdin);
+    n = write(sockfd,"enc",3);
+    if (n < 0) 
+         error("ERROR writing to socket");
+    bzero(buffer,256);
+    n = read(sockfd,buffer,255);
+    if (n < 0) 
+         error("ERROR reading from socket");
+         
+    //here we have new port in buffer so we close old connection
+    ///printf("New port:%s\n",buffer);
+    close(sockfd);
+    
+    int splitPort = atoi(buffer);
+    ///printf("New port int: %i\n", splitPort);
+    
+    //connect to the new port on the new process
+    //THIS MIGHT BE UNNECESSARY
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) 
+        error("ERROR opening new socket");
+    server = gethostbyname("localhost");
+    if (server == NULL) {
+        fprintf(stderr,"ERROR, no such new host\n");
+        exit(0);
+    }
+    
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    bcopy((char *)server->h_addr, 
+         (char *)&serv_addr.sin_addr.s_addr,
+         server->h_length);
+    serv_addr.sin_port = htons(splitPort);
+    if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
+        error("ERROR connecting");
+    /*printf("Please enter the second message: ");
+    bzero(buffer,256);
+    fgets(buffer,255,stdin);
+    n = write(sockfd,buffer,strlen(buffer));
+    if (n < 0) 
+         error("ERROR writing to socket");
+    bzero(buffer,256);
+    n = read(sockfd,buffer,255);
+    if (n < 0) 
+         error("ERROR reading from socket");
+    
+    //return msg
+    printf("Cli Received:%s\n",buffer); */
+    
+    /////////////////////////Send For Encryption//////////////////
+    
+    
+   
+	
+	//open CYPHER to write
+	//ERROR Do we need to clear/initiate this file prior
+	//to jumping into the loop?
+	int bytesCypher = 0;
+    ///FILE* cypherFile = fopen("cypherfile", "a");
+    ///if(cypherFile == NULL){
+		///perror("Can't open CYPHER file");
+		///exit(1);
+	///}
+	
+	//read PLAINTEXT in sections
+		bzero(buffer,256);
+		bytesRead = fread(buffer, 1, 255, inFile);
+		
+    do{
+		//to cut off the trailing newline;
+		if(buffer[bytesRead - 1] == '\n'){
+			buffer[bytesRead - 1] = '\0';
+			bytesRead--;
+		}
+		
+		//send the read line
+		///printf("C:Read:%s\n",buffer);
+		n = write(sockfd,buffer,strlen(buffer));
+		if (n < 0) 
+         error("ERROR writing to socket");
+         
+         
+         
+         //read KEY in sections
+		bzero(buffer,256);
+		bytesKey = fread(buffer, 1, bytesRead, keyFile);
+		
+		//on shorter tasks server needs to catchup.
+		sleep(1);
+       
+         //send the key line
+		///printf("C:Key:%s\n",buffer);
+		n = write(sockfd,buffer,strlen(buffer));
+		if (n < 0) 
+         error("ERROR writing to socket");
+        
+        //read encoded feedback
+        bzero(buffer,256);
+		n = read(sockfd,buffer,255);
+		if (n < 0) 
+         error("ERROR reading from socket");
+	
+		///printf("C:Encoded:%s\n",buffer);
+		//write CYPHER to file of OUT
+		///bytesCypher = fprintf(cypherFile,"%s",buffer);
+		bytesCypher = fprintf(stdout,"%s",buffer);
+		
+		//get next part
+		bzero(buffer,256);
+		bytesRead = fread(buffer, 1, 255, inFile);
+	}
+	while(bytesRead > 0);
+	
+	 bytesCypher = fprintf(stdout,"\n");   
+	
+	//printf("done.\n");
+	//send that we are done
+	n = write(sockfd,"done",4);
+	if (n < 0) 
+	 error("ERROR writing to socket");
+
+    //close files
+    fclose(inFile);
+    fclose(keyFile);
+    ///fclose(cypherFile);
+    //close connection
+    close(sockfd);
+    
+    return 0;
+}
---
+++ Assignment 4/otp_enc_d.c
@@ -0,0 +1,245 @@
+/* A simple server in the internet domain using TCP
+   The port number is passed as an argument 
+   This version runs forever, forking off a separate 
+   process for each connection
+*/
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h> 
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <ctype.h> //for iscntrl()
+#include <stdlib.h> //for random()
+
+void dostuff(int); /* function prototype */
+void error(const char *msg)
+{
+    perror(msg);
+    exit(1);
+}
+
+//returns the remainder of a devision.
+//not sure why % couldn't be used
+//but specifications called for this.
+int modNum(int numIn, int modNum){
+   while(numIn < 0){
+      numIn += modNum;
+   }
+   while(numIn >= modNum){
+      numIn -= modNum;
+   }
+
+   return numIn;
+}
+
+int main(int argc, char *argv[])
+{	//setup
+     int sockfd, newsockfd, portno, pid;
+     socklen_t clilen;
+     struct sockaddr_in serv_addr, cli_addr;
+	
+	//cmd line error check
+     if (argc < 2) {
+         fprintf(stderr,"ERROR, no port provided\n");
+         exit(1);
+     }
+     
+	//new socket
+     sockfd = socket(AF_INET, SOCK_STREAM, 0);
+     if (sockfd < 0) 
+        error("ERROR opening socket");
+     bzero((char *) &serv_addr, sizeof(serv_addr));
+     portno = atoi(argv[1]);
+     serv_addr.sin_family = AF_INET;
+     serv_addr.sin_addr.s_addr = INADDR_ANY;
+     serv_addr.sin_port = htons(portno);
+     
+     //bind the socket to port that was passed in
+     if (bind(sockfd, (struct sockaddr *) &serv_addr,
+              sizeof(serv_addr)) < 0) 
+              error("ERROR on binding");
+     listen(sockfd,5);
+     clilen = sizeof(cli_addr);
+     
+     int splitPort = portno;
+     
+     while (1) {
+		 //accept new connection
+         newsockfd = accept(sockfd, 
+               (struct sockaddr *) &cli_addr, &clilen);
+         if (newsockfd < 0) 
+             error("ERROR on accept");
+         
+         printf("S:newcon Accepted");
+         
+         //setup new port listener/new socket
+        splitPort++;
+        
+		int splitSockFD, newSplitSocFD;
+		socklen_t splitClilen;
+		struct sockaddr_in splitServ_addr, splitCli_addr;
+     
+         splitSockFD = socket(AF_INET, SOCK_STREAM, 0);
+		 if (splitSockFD < 0) 
+		 error("ERROR opening split socket");
+		 bzero((char *) &splitServ_addr, sizeof(splitServ_addr));
+		 splitServ_addr.sin_family = AF_INET;
+		 splitServ_addr.sin_addr.s_addr = INADDR_ANY;
+		 splitServ_addr.sin_port = htons(splitPort);
+		 
+		 //bind the socket to port that was passed in
+		if (bind(splitSockFD, (struct sockaddr *) &splitServ_addr,
+              sizeof(splitServ_addr)) < 0) 
+              error("ERROR on split binding");
+		listen(splitSockFD,1);
+		splitClilen = sizeof(splitCli_addr);
+         
+         //send back the new port number if enc "received"
+         //otherwise accept connection and drop it.
+         int n;
+		 char buffer[256];
+			  
+		 bzero(buffer,256);
+		 n = read(newsockfd,buffer,255);
+		 if (n < 0) error("ERROR reading from socket");
+		 printf("S:Here is the message: %s\n",buffer);
+		 
+		 if(buffer[0] == 'e'){
+		 char portConv[6];
+		 sprintf(portConv, "%d\n", splitPort);
+		 
+		 //send new port
+		 n = write(newsockfd,portConv, strlen(portConv) + 1);
+		 if (n < 0) error("ERROR writing to socket");
+		 
+		 } else {
+			 perror("S:Wrong encoding key.");
+			 newSplitSocFD = accept(splitSockFD, 
+               (struct sockaddr *) &splitCli_addr, &splitClilen);
+				if (newSplitSocFD < 0) 
+				error("ERROR on new accept");
+			close(newSplitSocFD);
+			continue; 
+		 }
+		 //accept new connection
+         newSplitSocFD = accept(splitSockFD, 
+               (struct sockaddr *) &splitCli_addr, &splitClilen);
+         if (newSplitSocFD < 0) 
+             error("ERROR on new accept");
+         
+         //split off a new process
+         pid = fork();
+         if (pid < 0)
+             error("ERROR on fork");
+         if (pid == 0)  {
+			 //child that takes the new connection with it
+             close(sockfd);
+             close(splitSockFD);
+             dostuff(newSplitSocFD);
+             close(newSplitSocFD);
+             exit(0);
+         }
+         //parent that closes the accepted connection and keeps listening.
+         else {
+			 close(newsockfd);
+			 close(splitSockFD);
+			 close(newSplitSocFD);
+		 }
+         
+     } /* end of while */
+     close(sockfd);
+     return 0; /* we never get here */
+}
+
+/******** DOSTUFF() *********************
+ There is a separate instance of this function 
+ for each connection.  It handles all communication
+ once a connnection has been established.
+ *****************************************/
+void dostuff (int sock)
+{
+	char key[256];
+	int n;
+	char buffer[256];
+	char plaintext[256];
+   
+      
+   /*bzero(buffer,256);
+   n = read(sock,buffer,255);
+   if (n < 0) error("ERROR reading from socket");
+   printf("S:Here is the message to new port: %s\n",buffer);
+   n = write(sock,"I got your message",18);
+   if (n < 0) error("ERROR writing to socket"); */
+   
+   //receiving PLAINTEXT then KEY the  send off CYPHER
+   int doneReading = 0;
+   do{
+	   //ERROR Add error check by length of shit taken in.
+	   //read PLAINTEXT or 'd' for DONE...
+	   bzero(buffer,256);
+	   n = read(sock,buffer,255);
+	   if (n < 0) error("ERROR reading from socket");
+	   
+	   ///printf("S: PLAIN?: %s", buffer);
+	   
+	   if(buffer[0] == 'd'){
+		   printf("S: done? %s", buffer);
+		   doneReading = 1;
+	   }else{
+		   
+		strncpy(plaintext, buffer, 256);
+	   
+	   //read section
+	   bzero(buffer,256);
+	   n = read(sock,buffer,255);
+	   if (n < 0) error("ERROR reading from socket");
+	   
+	   ///printf("S: KEY?: %s", buffer);
+	   
+	   if(buffer[0] == 'd'){
+		   printf("S: done? %s", buffer);
+		   doneReading = 1;
+	   }
+	   
+	   strncpy(key, buffer, 256);
+		}	   
+	   
+	   //encode received data HERE//////////////////////////
+	   //loop through chars, convert to num
+		int i = 0;
+		while(isprint(plaintext[i])){
+		   if(plaintext[i] == 32){ //32 is space
+			  plaintext[i] = 91; //91 because that is the next char after capitals
+		   }
+		   if(key[i] == 32){
+			  key[i] = 91;
+		   }
+		   int msgChar = plaintext[i] - 'A';
+		   int keyChar = key[i] - 'A';
+		//combine key char->num and msg char->num
+		   int cypherNum = msgChar + keyChar;
+		//mod the number
+		   int cypherChar = modNum(cypherNum, 27) + 'A';
+		   if(cypherChar == 91){
+			  cypherChar = 32;
+		   }
+		///printf("%c",cypherChar);
+		   buffer[i]  = cypherChar;
+
+		   i++;
+		}
+	   
+	   ///printf("S: Encoded: %s\n", buffer);
+	   
+	   //send encoded data back
+	   n = write(sock,buffer,strlen(buffer));
+	   if (n < 0) error("ERROR reading from socket");
+	   
+	   bzero(key,256);
+	   bzero(plaintext,256);
+   }
+   while(!doneReading);
+   
+}
